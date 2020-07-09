43e5949fe87f8928abc9b285d5b9da0ddcc1e1e5
mikpusch <pusch@ge.ibf.cnr.it>
2020-07-09 21:36:03 +0200

    20076

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
